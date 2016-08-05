21bb281f094103dbd41a4e0b8035cf2c1ff27296
jafow <jared.a.fowler@gmail.com>
2016-08-05 00:24:05 -0700

    TaskPanel renders appropriate task in corresponding panel

--- components/TaskPanel.js
+++ components/TaskPanel.js
@@ -4,15 +4,31 @@ import Task from './Task.js'
 
 class TaskPanel extends Component {
   render() {
-    if (this.props.tasks) {
-      var tasks = this.props.tasks.map((task, i) =>
-          <Task
+    // render completed tasks in Completed panel, pending tasks in pending
+    var tasks
+    if (/progress/.test(this.props.tile)) {
+      tasks = this.props.tasks.filter((task, i) => {
+        if (!task.isComplete) {
+          return (
+              <Task
           name={task.name}
           description={task.description}
           key={i}
           />
-          )
-    } 
+          )}
+      })
+    } else {
+      tasks = this.props.tasks.filter((task, i) => {
+        if (task.isComplete) {
+          return (
+              <Task
+          name={task.name}
+          description={task.description}
+          key={i}
+          />
+          )}
+      })
+    }
     return (
         <div>
           <h2>{this.props.title}</h2>
